341cadf2e25370790c1a3600a0e4fd8275eac5a8
LTS <DKLTSghost@gmail.com>
2022-01-15 07:32:41 +0800

    modify

--- jstest/prime.py
+++ jstest/prime.py
@@ -1,13 +1,13 @@
 
 
 def prime(rg):
-    for i in range(2,rg+1):s
+    for i in range(2,rg+1):                    # 2~rg
         if i==2:
             print(i)
             continue
         elif i%2 == 0: continue
         else:
-            for j in range(2,i):
+            for j in range(2,i):              # 2~rg(odd)
                 if i%j==0: break
                 elif j+1 == i: print(i)
 
--- jstest/prune.py
+++ jstest/prune.py
@@ -1,22 +1,27 @@
 
 
-
 def prune(rope,meter):
-    return prune(rope / 2 ,meter) + 1 if rope > meter else 0
+    if rope > meter :
+        rope /= 2
+        return prune(rope,meter) + 1
+    return 0
+
+    # return prune(rope / 2, limit_length) + 1 if rope > limit_length else 0
+
+# def Cir_stop(m):
+#     if (m > 3):
+#         return Cir_stop(m-1)+1
+#     else:
+#         return 1
 
 
 
 
 
-
-
-
-
-
-
-
-if __name__ == "__main__":
+if __name__ == "__main__" :
     rope = 3000
     meter = 5
     day = prune(rope,meter)
     print(day)
+    # Cir_stop(meter)
+    
